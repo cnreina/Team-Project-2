f035326f07035dcf3022a5fd8f564904b0073f0c
Carlos N Reina <cnreina@gmail.com>
2022-02-07 20:22:47 -0700

    refactored user tasks view

--- public/js/user.js
+++ public/js/user.js
@@ -1,7 +1,7 @@
 const deleteTask = btn => {
   const taskId      = btn.parentNode.querySelector('[name=taskId]').value;
   const csrf        = btn.parentNode.querySelector('[name=_csrf]').value;
-  const taskElement = btn.closest('article');
+  const taskElement = btn.closest('ul');
 
   fetch('/user/delete-task/' + taskId, {
     method: 'DELETE',
@@ -17,5 +17,49 @@ const deleteTask = btn => {
     })
     .catch(err => {
       console.log('deleteTask ERROR: ', err);
+    });
+};
+
+const punchIn = btn => {
+  const taskId      = btn.parentNode.querySelector('[name=taskId]').value;
+  const csrf        = btn.parentNode.querySelector('[name=_csrf]').value;
+  const taskElement = btn.closest('article');
+
+  fetch('/user/punchin/' + taskId, {
+    method: 'POST',
+    headers: {
+      'csrf-token': csrf
+    }
+  })
+    .then(result => {
+      return result.json();
+    })
+    .then(data => {
+      taskElement.parentNode.removeChild(taskElement);
+    })
+    .catch(err => {
+      console.log('punchIn ERROR: ', err);
+    });
+};
+
+const punchOut = btn => {
+  const taskId      = btn.parentNode.querySelector('[name=taskId]').value;
+  const csrf        = btn.parentNode.querySelector('[name=_csrf]').value;
+  const taskElement = btn.closest('article');
+
+  fetch('/user/punchout/' + taskId, {
+    method: 'POST',
+    headers: {
+      'csrf-token': csrf
+    }
+  })
+    .then(result => {
+      return result.json();
+    })
+    .then(data => {
+      taskElement.parentNode.removeChild(taskElement);
+    })
+    .catch(err => {
+      console.log('punchOut ERROR: ', err);
     });
 };
