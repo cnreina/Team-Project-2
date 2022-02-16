8cfcfe4d6bf9a1e25ca20c8e7d44aaa8f4b5c6b7
Carlos N Reina <cnreina@gmail.com>
2022-02-15 20:02:11 -0700

    css

--- public/js/user.js
+++ public/js/user.js
@@ -1,3 +1,9 @@
+setInterval(userTimer, 1000);
+function userTimer() {
+  const elapsedTime = new Date();
+  document.getElementById("timedisplay").innerHTML = elapsedTime.toLocaleTimeString();
+}
+
 const deleteTask = btn => {
   const taskId      = btn.parentNode.querySelector('[name=taskId]').value;
   const csrf        = btn.parentNode.querySelector('[name=_csrf]').value;
@@ -17,27 +23,5 @@ const deleteTask = btn => {
     })
     .catch(err => {
       console.log('deleteTask ERROR: ', err);
-    });
-};
-
-const archiveTask = btn => {
-  const taskId      = btn.parentNode.querySelector('[name=taskId]').value;
-  const csrf        = btn.parentNode.querySelector('[name=_csrf]').value;
-  const taskElement = btn.closest('ul');
-
-  fetch('/user/archive-task/' + taskId, {
-    method: 'POST',
-    headers: {
-      'csrf-token': csrf
-    }
-  })
-    .then(result => {
-      return result.json();
-    })
-    .then(data => {
-      taskElement.parentNode.removeChild(taskElement);
-    })
-    .catch(err => {
-      console.log('archiveTask ERROR: ', err);
     });
 };
